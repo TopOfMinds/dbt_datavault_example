7e7341b11396e5bdb128f31c2b96cdd3823c8617
Philip Hägglund <philip.hagglund@topofminds.se>
2022-12-06 16:21:37 +0100

    Driving key macro for customer_customer_class_l_sb

--- models/bv/customer_customer_class_l_sb.sql
+++ models/bv/customer_customer_class_l_sb.sql
@@ -1,9 +1,16 @@
-SELECT
-  l.customer_customer_class_l_key
-  ,GREATEST(l.load_dts, s.load_dts) AS load_dts
-  ,s.effective_ts
-  ,COALESCE(LEAD(s.effective_ts) OVER(PARTITION BY l.customer_key ORDER BY s.effective_ts), TIMESTAMP('9999-12-31')) AS effective_ts_end
-  ,s.rec_src
-FROM  {{ ref('customer_customer_class_l') }} l
-LEFT JOIN  {{ ref('customer_customer_class_l_s') }} s on s.customer_customer_class_l_key = l.customer_customer_class_l_key
+{% set metadata_yaml -%}
+target: 
+  link_key: customer_customer_class_l_key
+  effective_ts: effective_ts
+link_source:
+    table: customer_customer_class_l
+    link_key: customer_customer_class_l_key
+    driving_key: customer_key
+    other_key: customer_class_key
+sat_source:
+    table: customer_customer_class_l_s
+    link_key: customer_customer_class_l_key
+    effective_ts: effective_ts
+{%- endset %}
 
+{{- dbt_datavault.driving_key_satellite(metadata_yaml) }}
